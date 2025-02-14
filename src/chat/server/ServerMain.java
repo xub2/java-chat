bfd3031a9ce6cfa22e9c4e20de34729022367cdd
xub2 <victory62808@gmail.com>
2025-02-14 23:02:18 +0900

    2/14 채팅 프로그램 최종 코드

--- src/chat/server/ServerMain.java
+++ src/chat/server/ServerMain.java
@@ -9,9 +9,10 @@ public class ServerMain {
         SessionManager sessionManager = new SessionManager();
 
         // CommandManager 점진적으로 변경 예정
-        //CommandManagerV1 commandManager = new CommandManagerV1(sessionManager);
+        //CommandManager = new CommandManagerV1(sessionManager);
         //CommandManager commandManager = new CommandManagerV2(sessionManager);
-        CommandManager commandManager = new CommandManagerV3(sessionManager);
+        //CommandManager commandManager = new CommandManagerV3(sessionManager);
+        CommandManager commandManager = new CommandManagerV4(sessionManager);
 
         Server server = new Server(PORT, commandManager, sessionManager);
         server.start();
